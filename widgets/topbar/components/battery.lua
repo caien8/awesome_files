af4ed679303c6822ce5ad91e26701ba2d9875c44
caien8 <caien811_P@proton.me>
2024-02-05 19:35:52 +0545

    battery widget

--- widgets/topbar/components/battery.lua
+++ widgets/topbar/components/battery.lua
@@ -1,8 +1,7 @@
 local awful = require("awful")
 local wibox = require("wibox")
-
 local battery_path = "/sys/class/power_supply/BATT/"
-local update_interval = 2
+local update_interval = 1
 
 --* Function to read battery percentage
 local function get_battery_percentage()
@@ -26,43 +25,73 @@ local function get_battery_status()
     return "Unknown"
 end
 
--- Function to update battery widget
-local function update_battery_widget(widget)
-    local capacity = get_battery_percentage()
-    local status = get_battery_status()
-
-    local battery_icon
+--* Function to get the battery icon
+local function get_battery_icon(status, capacity)
     if status == "Charging" then
-        battery_icon = "" -- You can replace this with your charging icon
+        icon = ""
+        color = "#00ff00"
     elseif capacity >= 80 then
-        battery_icon = "" -- Battery full icon
+        icon = ""
+        color = "#00ff00"
     elseif capacity >= 60 then
-        battery_icon = "" -- Battery 3/4 icon
+        icon = ""
+        color = "#ffff00"
     elseif capacity >= 40 then
-        battery_icon = "" -- Battery 1/2 icon
+        icon = ""
+        color = "#ffa500"
     elseif capacity >= 20 then
-        battery_icon = "" -- Battery 1/4 icon
+        icon = ""
+        color = "#ff4500"
     else
-        battery_icon = "" -- Battery empty icon
+        icon = ""
+        color = "#ff0000"
     end
-
-    widget.markup = '<span font="Roboto Medium 12">' .. battery_icon .. ' ' .. capacity .. '%</span>'
+    return icon, color
 end
 
--- Create battery widget
-local battery_widget = wibox.widget {
-    widget = wibox.widget.textbox
+--* Create battery icon widget
+local battery_icon_widget = wibox.widget {
+    widget = wibox.widget.textbox,
+    font = "Roboto Medium 20"
 }
 
--- Update battery widget initially
-update_battery_widget(battery_widget)
+--* Create battery capacity widget
+local battery_capacity_widget = wibox.widget {
+    widget = wibox.widget.textbox,
+    font = "Roboto Medium 10"
+}
 
--- Update battery widget every 60 seconds
-awful.widget.watch("bash -c 'cat /sys/class/power_supply/BATT/capacity /sys/class/power_supply/BATT/status'", update_interval, 
-    function(widget, stdout)
-        update_battery_widget(widget)
-    end,
-    battery_widget
+--* Function to update battery icon widget
+local function update_battery_icon_widget()
+    local status = get_battery_status()
+    local capacity = get_battery_percentage()
+    local battery_icon = get_battery_icon(status, capacity)
+    battery_icon_widget.markup = '<span color = "' ..color.. '" >' .. battery_icon .. '</span>'
+end
+
+--* Function to update battery capacity widget
+local function update_battery_capacity_widget()
+    local capacity = get_battery_percentage()
+    battery_capacity_widget.markup = '<span>' .. capacity .. '%</span>'
+end
+
+--* Update battery widgets initially
+update_battery_icon_widget()
+update_battery_capacity_widget()
+
+--* Update battery widgets on regular interval
+awful.widget.watch("bash -c 'cat ${battery_path}/capacity ${battery_path}/status'", update_interval,
+    function()
+        update_battery_icon_widget()
+        update_battery_capacity_widget()
+    end
 )
+
+--* Combine battery icon and capacity widgets into one battery widget
+local battery_widget = wibox.widget {
+    layout = wibox.layout.fixed.horizontal,
+    battery_icon_widget,
+    battery_capacity_widget
+}
 
 return battery_widget
